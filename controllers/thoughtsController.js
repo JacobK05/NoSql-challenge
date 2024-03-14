b515e8acf50dec5ac4bb75c5d96dc79afa0c566d
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-03-14 14:20:36 -0500

    finished the thought controller

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,81 @@
+const { ObjectId } = require('mongoose').Types;
+const { Thought, User } = require('../models');
+
+
+module.exports = {
+// get all thoughts
+async getThoughts(req, res){
+  try{
+    const thoughts = await Thought.find()
+    res.json(thoughts)
+  }catch (err){
+    console.log(err)
+    res.status(500).json(err)
+  }
+},
+//get a single thought
+async getSingleThought(req, res){
+  try{ 
+    const thought = await Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+
+  if (!thought){
+    return res.status(404).json({ message: 'no thought with this id'})
+  }
+
+  res.json(thought)
+  }catch (err){
+    console.log(err)
+    return res.status(500).json(err)
+  }
+},
+// create a new thought 
+async createThought(req, res){
+  try{
+    const thought = await Thought.create(req.body)
+
+    const user = await User.findOneAndUpdate(
+      {_id: req.body.userId},
+      {$push: { thoughts: thought.Id}},
+      {new: true}
+    )
+    res.json(thought)
+  } catch(err){
+    console.log(err)
+    return res.status(500).json(err)
+  }
+},
+// update a thought 
+async updateThought (req, res) {
+  try{ 
+    const thought = await Thought.findOneAndUpdate(
+      {_id: req.body.thoughtId},
+      {$set: req.body},
+      { runValidators: true, new: true }
+    )
+  if (!thought){
+    return res.status(404).json({message: 'no thought with this id'})
+  }
+
+res.json('thought deleted')
+  }catch (err) {
+    console.log(err)
+    return res.status(500).json(err)
+  }
+},
+// delete a thought
+async deleteThought(req, res){
+  try{
+  const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId});
+
+  if(!thought){
+    return res.status(404).json({ message: 'no thought with this id'})
+  }
+  res.json(thought)
+  }catch (err){
+    console.log(err)
+    return res.status(500).json(err)
+  }
+}
+
+}
